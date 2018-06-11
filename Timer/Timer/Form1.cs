7ee943abcc26fa5dbbdc299ba234764746278b1e
snoowty <tanosiienjoy@gmail.com>
2018-06-12 02:59:24 +0900

    Set and get time

---
+++ Timer/Timer/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Time time1 = new Time();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            time1.Hour = int.Parse(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            time1.Minute = int.Parse(textBox2.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            time1.Second = int.Parse(textBox3.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.DateTime currentTime = System.DateTime.Now;
+            Time time2 = new Time(currentTime);
+
+            time2.DisplayTime();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            time1.DisplayTime();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Timer/Timer/Time.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    class Time
+    {
+        int hour, minute, second;
+
+        public Time()
+        {
+            hour = 0;
+            minute = 0;
+            second = 0;
+        }
+
+        public Time(System.DateTime dt)
+        {
+            hour = dt.Hour;
+            minute = dt.Minute;
+            second = dt.Second;
+        }
+
+        public void DisplayTime()
+        {
+            MessageBox.Show(hour + "時" + minute + "分" + second + "秒");
+        }
+
+        public int Hour
+        {
+            set
+            {
+                if (0 <= value && value <= 24)
+                    hour = value;
+                else
+                    MessageBox.Show("入力された値が不適切です。");
+            }
+        }
+
+        public int Minute
+        {
+            set
+            {
+                if (0 <= value && value < 60)
+                    minute = value;
+                else
+                    MessageBox.Show("入力された値が不適切です。");
+            }
+        }
+
+        public int Second
+        {
+            set
+            {
+                if (0 <= value && value < 60)
+                    second = value;
+                else
+                    MessageBox.Show("入力された値が不適切です。");
+            }
+        }
+    }
+}
